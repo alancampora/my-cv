c36477ea074748792e995756acfd2f94cbdd4e50
Alan Campora <alan.campora@gmail.com>
2018-03-26 09:33:54 -0300

    [MOD] Using bubble poits instead of photos

--- src/components/BubbleData/BubbleData.js
+++ src/components/BubbleData/BubbleData.js
@@ -1,21 +1,26 @@
 import React from 'react';
 
-const BubbleData = ({company, position, description, date, photo}) => (
-    <div className="BubbleData">
-        <div className="BubbleData-Left">
-            <div className="BubbleData-Photo">
-                <img className="BubbleData-Photo-Img" src={photo} />
+const BubbleData = ({company, position, description, date, photo}) => {
+    let formattedDates = {
+        from: new Date(date.from),
+        to: new Date(date.to)
+    };
+    return (
+        <div className="BubbleData">
+            <div className="BubbleData-Left">
+                <div className="BubbleData-Bubble" />
+                <div className="BubbleData-Line" />
             </div>
-        </div>
 
-        <div className="BubbleData-Right">
-            <div className="BubbleData-Header">
-                {' '}
-                {date.from} - {date.to} : {position} @ {company}
+            <div className="BubbleData-Right">
+                <div className="BubbleData-Header">
+                    {' '}
+                    {date.from} - {date.to} : {position} @ {company}
+                </div>
+                <div className="BubbleData-Description">{description} </div>
             </div>
-            <div className="BubbleData-Description">{description} </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default BubbleData;
